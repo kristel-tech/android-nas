71901f3beea9a2798e0b33e612d793ad6257e067
khutjo <kmaputla@student.wethinkcode.co.za>
2021-04-19 23:12:21 +0200

    add main for testing and some prepard sql

--- Database/src/NASDatabase.java
+++ Database/src/NASDatabase.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -8,9 +9,9 @@ import java.time.LocalDateTime;
 
 
 public class NASDatabase {
-    private static final String DatabaseURL = "jdbc:mysql://localhost:<port number>/<DB name>";
-    private static final String user = "user";
-    private static final String password = "password";
+    private static final String DatabaseURL = "jdbc:mysql://localhost:3306/java";
+    private static final String user = "root";
+    private static final String password = "";
     private static Connection connection;
     private static Statement statement;
     private static ResultSet resultset;
@@ -21,6 +22,7 @@ public class NASDatabase {
 
     public void ShowFilesByDeviceID(String DeviceIPAddress, String OwnIPAddress) throws SQLException {
         try {
+            Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection(DatabaseURL, user, password);
             statement = connection.createStatement();
             String SQLQuery = "SELECT * FROM FileHistory WHERE (SenderIP = " + DeviceIPAddress + " AND ReceiverIP = " + OwnIPAddress + ") OR ReceiverIP = " + DeviceIPAddress + " AND SenderIP = " + OwnIPAddress + ";";
@@ -30,6 +32,8 @@ public class NASDatabase {
             }
         } catch (SQLException sqlEx) {
             sqlEx.printStackTrace();
+        }catch(Exception e){
+            System.out.println("we are not flying");
         }
         finally {
             closeConnection();
@@ -76,16 +80,21 @@ public class NASDatabase {
     public void CreateFileRecord(String SenderIP, String ReceiverIP, double FileSizeInMegabytes, LocalDateTime local, String filename) throws SQLException {
        try {
            connection = DriverManager.getConnection(DatabaseURL, user, password);
-           statement = connection.createStatement();
-           String SQLQuery = "INSERT INTO FileHistory(SenderIP,ReceiverIP,FileSizeInMegabytes,DateSent,FileName) VALUES(" + SenderIP + ", " + ReceiverIP + ", " + FileSizeInMegabytes + ", " + local + ", " + filename + ");";
-           statement.executeUpdate(SQLQuery);
+           
+           String SQLQuery =  "INSERT INTO filehistory(SenderIP, ReceiverIP, FileSizeInMegabytes, DateSent, FileName) VALUES (?,?,?,CURRENT_TIMESTAMP,?);";
+           
+           PreparedStatement updatefilehistory = connection.prepareStatement(SQLQuery);
+           updatefilehistory.setString(1, SenderIP);
+           updatefilehistory.setString(2, ReceiverIP);
+           updatefilehistory.setDouble(3, FileSizeInMegabytes);
+           updatefilehistory.setString(4, filename);
+           updatefilehistory.executeUpdate();
+           
        }
        catch (SQLException sqlEx) {
            sqlEx.printStackTrace();
        }
-       finally {
-           closeConnection();
-       }
+
     }
     public void closeConnection() {
         try {
---
+++ Database/src/home.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDateTime;
+
+
+public class home {
+	
+    public static void main(String args[]) throws Exception {
+
+    	NASDatabase hold = new NASDatabase();
+
+        //public void CreateFileRecord(String SenderIP, String ReceiverIP, double FileSizeInMegabytes, LocalDateTime local, String filename) throws SQLException 
+        try{
+        hold.CreateFileRecord("tttttt", "ttttt", 3.3, LocalDateTime.now(), "bihhhh");
+        }
+        catch(Exception e ){
+            System.out.println("shit happans");
+            System.out.println(e);
+        }
+        System.out.println("hello");
+
+
+    }
+
+}
